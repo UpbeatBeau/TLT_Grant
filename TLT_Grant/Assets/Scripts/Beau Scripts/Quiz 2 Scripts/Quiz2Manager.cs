b45d7c762db13d63c64b276c3de67154389fd544
UpbeatBeau <70418283+UpbeatBeau@users.noreply.github.com>
2025-02-24 12:00:51 -0600

    Making Quiz 2

---
+++ TLT_Grant/Assets/Scripts/Beau Scripts/Quiz 2 Scripts/Quiz2Manager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Quiz2Manager : MonoBehaviour
+{
+    //add the text and the scriptable objects
+    public TextMeshProUGUI qtext;
+    public Questions_SO current_question;
+    public Questions_SO next_question;
+    public GameObject[] buttons;
+    public GameObject nextbutton;
+    public GameObject input;
+   
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        qtext.text = current_question.questiontxt;
+        input.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //write a function to update which question is showing
+    public void Updatequiz()
+    {
+        if (current_question.lastquestion == false)
+        {
+            //change the text to the current question text
+            qtext.text = next_question.questiontxt;
+            current_question = next_question;
+            next_question = current_question.nextquestion;
+
+        }
+        else {
+            qtext.text = current_question.questiontxt;
+            
+        }
+        
+        
+    }
+   public void ButtonOff()
+    {
+        foreach (GameObject btn in buttons)
+        {
+            btn.SetActive(false);
+        }
+        input.SetActive(true);
+
+    }
+
+    public void ClearInput()
+    {
+      
+        input.GetComponent<TMP_InputField>().text = "";
+    }
+}
